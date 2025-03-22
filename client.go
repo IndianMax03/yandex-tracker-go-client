28097ba3edc98015f03eb874813d97f7d9b5b0e2
IndianMax <58441613+IndianMax03@users.noreply.github.com>
2025-03-22 23:09:13 +0300

    Add: base client
    * Fix: README command & Add: pkg.go.dev reference
    
    * Fix: pkg.go.dev reference
    
    * Fix: pkg.go.dev reference # 2
    
    * Fix: pkg.go.dev reference # 3
    
    * Fix: license reference
    
    * Add: go & md workflows
    
    * Add: client base realisation
    
    * Fix: md lint
    
    * Add: docs

--- client.go
+++ client.go
@@ -1,14 +1,57 @@
+// The client package provides methods for interacting with Yandex Tracker
 package client
 
-import "fmt"
+import "resty.dev/v3"
 
-type Client struct{}
+const (
+	baseURL            = "https://api.tracker.yandex.net/v2/"
+	defaultContentType = "application/json"
+	defaultLang        = "ru"
+	defaultAuthScheme  = "OAuth"
+)
 
-func New() (c *Client) {
-	return &Client{}
+// Client is a wrapper over the resty.Client type with Yandex Tracker API-specific headers and a base URL
+type Client struct {
+	restyClient *resty.Client
 }
 
-func (c *Client) Greet(name string) (greet string) {
-	greet = fmt.Sprintf("Greeting, %s!", name)
+// Create a new Client
+func New(tokenOAuth, xCloudOrgID, xOrgID, acceptLanguage string) *Client {
+	var lang string
+	headers := map[string]string{}
+
+	if acceptLanguage != "ru" && acceptLanguage != "en" {
+		lang = defaultLang
+	}
+	headers["Accept-Language"] = lang
+
+	if xOrgID != "" {
+		headers["X-Org-ID"] = xOrgID
+	} else if xCloudOrgID != "" {
+		headers["X-Cloud-Org-ID"] = xCloudOrgID
+	}
+
+	restyClient := resty.New()
+	restyClient.SetHeaders(headers)
+	restyClient.SetAuthScheme(defaultAuthScheme)
+	restyClient.SetAuthToken(tokenOAuth)
+	restyClient.SetBaseURL(baseURL)
+
+	return &Client{
+		restyClient: restyClient,
+	}
+}
+
+// Send request to Yandex Tracker
+func (c *Client) SendRequest(method, resourceURL string, requestBody, responseBody any) (resp *resty.Response, err error) {
+	req := c.restyClient.R().
+		SetContentType(defaultContentType).
+		SetMethod(method).
+		SetBody(requestBody).
+		SetResult(responseBody).
+		SetURL(c.restyClient.BaseURL() + resourceURL)
+
+	req.SetDebug(true)
+	resp, err = req.Send()
 	return
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/IndianMax03/yandex-tracker-go-client
 
 go 1.24.1
+
+require resty.dev/v3 v3.0.0-beta.2
+
+require golang.org/x/net v0.37.0 // indirect
