1a4a6ceba654a8a8ef65a67ef3cb2ed2208e2699
IndianMax03 <vermamanu@mail.ru>
2025-03-28 00:42:32 +0300

    Add: issue searching & Fix: issue creating

--- models.go
+++ models.go
@@ -82,8 +82,10 @@ type IssueCreateResponse struct {
 	Assignee IssueAssignee `json:"assignee"`
 	// Number of votes for the issue.
 	Votes int `json:"votes"`
-	// Number of comments without external message
+	// Number of comments with external message
 	CommentWithExternalMessageCount int `json:"commentWithExternalMessageCount"`
+	// Number of comments without external message
+	CommentWithoutExternalMessageCount int `json:"commentWithoutExternalMessageCount"`
 	// An object with information about a issue queue.
 	Queue IssueQueue `json:"queue"`
 	// Date and time the issue was updated.
@@ -110,9 +112,90 @@ type IssueCountRequest struct {
 // Number of issues
 type IssueCountResponse int
 
+// Request to get filtered and sorted tasks
+type IssueSearchRequest struct {
+	Filter map[string]any `json:"filter,omitempty"`
+	Order  string         `json:"order,omitempty"`
+	Query  string         `json:"query,omitempty"`
+}
+
+// An array of objects containing information about issues
+type IssueSearchResponse []struct {
+	// The address of the API resource that contains information about the issue.
+	Self string `json:"self"`
+	// Issue ID.
+	ID string `json:"id"`
+	// Issue Key.
+	Key string `json:"key"`
+	// Issue version. Each change to the issue parameters increases the version number.
+	// Editing the issue will be blocked if the version reaches the maximum value: for robots 10100, for users 11100.
+	Version int `json:"version"`
+	// Date and time of the last comment added.
+	//LastCommentUpdatedAt string `json:"lastCommentUpdatedAt"`
+	// Issue name.
+	Summary string `json:"summary"`
+	// No info: https://yandex.ru/support/tracker/ru/concepts/issues/search-issues
+	Boards []Board `json:"boards"`
+	// No info: https://yandex.ru/support/tracker/ru/concepts/issues/search-issues
+	StatusStartTime string `json:"statusStartTime"`
+	// An object with information about the parent issue.
+	// Parent IssueParent `json:"parent"`
+	// An object containing information about the last employee who modified the issue.
+	UpdatedBy IssueUpdatedBy `json:"updatedBy"`
+	// Description of the issue.
+	// Description string `json:"description"`
+	// An array of objects containing information about the sprint.
+	// Sprint IssueSprint `json:"sprint"`
+	// An object with information about the issue type.
+	Type IssueType `json:"type"`
+	// Object with priority information.
+	Priority IssuePriority `json:"priority"`
+	// Date and time the issue was created.
+	CreatedAt string `json:"createdAt"`
+	// An array of objects containing information about the issue's observers.
+	// Followers IssueFollowers `json:"followers"`
+	// An object containing information about the issue creator.
+	CreatedBy IssueCreatedBy `json:"createdBy"`
+	// An object with information about the issue performer.
+	// Assignee IssueAssignee `json:"assignee"`
+	// Number of votes for the issue.
+	Votes int `json:"votes"`
+	// Number of comments with external message
+	CommentWithExternalMessageCount int `json:"commentWithExternalMessageCount"`
+	// Number of comments without external message
+	CommentWithoutExternalMessageCount int `json:"commentWithoutExternalMessageCount"`
+	// An object with information about a issue queue.
+	Queue IssueQueue `json:"queue"`
+	// Date and time the issue was updated.
+	UpdatedAt string `json:"updatedAt"`
+	// An object with information about the issue status.
+	Status IssueStatus `json:"status"`
+	// An object with information about the issue status type.
+	StatusType IssueStatusType `json:"statusType"`
+	// Flag of a favorite issue:
+	// true — the user added the issue to favorites;
+	// false — the issue was not added to favorites.
+	Favorite bool `json:"favorite"`
+}
+
+type PageRequest struct {
+	PerPage int
+	Page    int
+}
+
+type PageResponse struct {
+	TotalPages int
+	TotalCount int
+}
+
 type Queue struct {
 	ID  string `json:"id"`
 	Key string `json:"key"`
+}
+
+type Board struct {
+	ID   any    `json:"id"`
+	Name string `json:"name"`
 }
 
 type ObjectBase struct {
