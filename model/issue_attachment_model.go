c64c3599924efe58ff0cdd3551ace69fbae49ced
IndianMax03 <vermamanu@mail.ru>
2025-04-04 00:51:27 +0300

    Fix: AttachmentFileResponse

--- model/issue_attachment_model.go
+++ model/issue_attachment_model.go
@@ -4,27 +4,27 @@ package model
 // AttachmentFileResponse describes response contains information about attachment.
 type AttachmentFileResponse struct {
 	// The address of the API resource that corresponds to the attached file.
-	Self string
+	Self string `json:"self"`
 	// Unique file identifier.
-	ID string
+	ID string `json:"id"`
 	// File name.
-	Name string
+	Name string `json:"name"`
 	// The address of the resource for downloading the file.
-	Content string
+	Content string `json:"content"`
 	// Resource address for downloading the preview thumbnail. Available only for graphic files.
-	Thumbnail string
+	Thumbnail string `json:"thumbnail"`
 	// An object containing information about the user who attached the file.
-	CreatedBy CreatedBy
+	CreatedBy CreatedBy `json:"createdBy"`
 	// Date and time of file upload in the format: YYYY-MM-DDThh:mm:ss.sss±hhmm
-	CreatedAt string
+	CreatedAt string `json:"createdAt"`
 	// File type, for example:
 	// text/plain — text file;
 	// image/png — png image.
-	Mimetype string
+	Mimetype string `json:"mimetype"`
 	// File size in bytes.
-	Size int
+	Size int `json:"size"`
 	// An object with file metadata.
-	Metadata FileMetadata
+	Metadata FileMetadata `json:"metadata"`
 }
 
 // FileMetadata describes object with file metadata.
